77d4d9edb7623eccfe9e1512e96d460c1cfba58f
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-10-23 12:59:33 +0200

    Knob is stoppable by Mouse Hold

--- Testaccio_Unity/Assets/Scripts/KnobMove.cs
+++ Testaccio_Unity/Assets/Scripts/KnobMove.cs
@@ -12,50 +12,79 @@ public class KnobMove : MonoBehaviour
     private float knobY;
     
     [SerializeField] private float rotationSpeed = 200;
-    private bool isCircling;
+    private bool stopCircling;
     private float angle;
     
     private float circleRadius;
+    private float knobRadius;
     private Vector2 centerPos;
     
     private RectTransform circleRectTransform;
+    private RectTransform knobRectTransform;
+
+    private KnobPlayerInput knobPlayerInput;
     
     // Start is called before the first frame update
     void Start()
     {
         centerPos = transform.position;
         
-        // Go get the Rect Transform Component
+        // Go get the Circle Rect Transform Component
         circleRectTransform = transform.GetComponent<RectTransform>();
+        
+        // Go get the Knob Rect Transform Component
+        knobRectTransform = knob.GetComponent<RectTransform>();
+        
+        // Links to other Scripts
+        knobPlayerInput = GetComponent<KnobPlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateCircleRadius();
+        CalculateKnobRadius();
+        GetPlayerInputData();
         CalculateRotationSpeed();
         KnobToCenterDistance();
         MoveKnob();
-        //AnimateWithTween();
+    }
+    
+    private void CalculateKnobRadius()
+    {
+        Vector3 localScale = knobRectTransform.localScale; // Get the local scale factor
+        float scaleX = localScale.x;
+        
+        // Calculate the Radius of the Circle that has this script attached
+        knobRadius = circleRectTransform.sizeDelta.x * 0.5f * scaleX; 
     }
 
     private void CalculateCircleRadius()
     {
+        // Calculate Big Circle Radius
         Vector3 localScale = circleRectTransform.localScale; // Get the local scale factor
         float scaleX = localScale.x;
         
         // Calculate the Radius of the Circle that has this script attached
         circleRadius = circleRectTransform.sizeDelta.x * 0.5f * scaleX; 
     }
+    
+    private void GetPlayerInputData()
+    {
+        stopCircling = knobPlayerInput.stopCircling;
+    }
 
     private void CalculateRotationSpeed()
     {
-         angle += Time.deltaTime * rotationSpeed;
+        if (stopCircling)
+            return;
+        
+        angle += Time.deltaTime * rotationSpeed;
          
-         if (angle > 360.0f)
-         {
-             angle -= 360.0f;
-         }
+        if (angle > 360.0f)
+        {
+            angle -= 360.0f;
+        }
     }
 
     private void KnobToCenterDistance()
@@ -69,13 +98,5 @@ public class KnobMove : MonoBehaviour
     private void MoveKnob()
     {
         knob.transform.position = new Vector3(knobX, knobY, 0);
-    }
-    
-    private void AnimateWithTween()
-    {
-        // Move using DoTween
-        // knob.DOMove(knobX, knobY, 20);
-         
-         
     }
 }
