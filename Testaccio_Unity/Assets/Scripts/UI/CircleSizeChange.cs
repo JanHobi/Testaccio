3c5393c124ffaa0645a4d4066e46afbe6ef1feb3
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-10-30 11:41:02 +0100

    Circle Size UI is working
    But does not change Real circle Size yes

--- Testaccio_Unity/Assets/Scripts/UI/CircleSizeChange.cs
+++ Testaccio_Unity/Assets/Scripts/UI/CircleSizeChange.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UI
@@ -8,32 +9,57 @@ namespace UI
         [HideInInspector] public float activeCircleSize ;
 
         [SerializeField] private float[] allCircleSizes = new float[4];
+
+        [SerializeField] private GameObject[] allButtonFillings = new GameObject[4];
+        
         // Start is called before the first frame update
         void Start()
         {
+            activeCircleSize = 1;
         }
-
-        // Update is called once per frame
-        void Update()
-        {
         
+        
+        // Deactivate all button fillings
+        private void DeactivateAllButtonFillings()
+        {
+            foreach (var filling in allButtonFillings)
+            {
+                filling.SetActive(false);
+            }
+        }
+        
+        // Activate the specific button filling and set activeCircleSize
+        private void ActivateButtonFilling(int index)
+        {
+            if (index >= 0 && index <= allButtonFillings.Length)
+            {
+                DeactivateAllButtonFillings();
+                Debug.Log("index: " + index);
+                allButtonFillings[index].SetActive(true); // Activate the specific button's filling
+                activeCircleSize = allCircleSizes[index]; // Set the new Size of the Circle
+            }
+            else
+            {
+                Debug.LogError("Index out of range: " + index);
+            }
+            
         }
 
         public void Size1()
         {
-            activeCircleSize = allCircleSizes[1];
+            ActivateButtonFilling(0);
         }
         public void Size2()
         {
-            activeCircleSize = allCircleSizes[2];;
+            ActivateButtonFilling(1);
         }
         public void Size3()
         {
-            activeCircleSize = allCircleSizes[3];;
+            ActivateButtonFilling(2);
         }
         public void Size4()
         {
-            activeCircleSize = allCircleSizes[4];;
+            ActivateButtonFilling(3);
         }
     }
 }
