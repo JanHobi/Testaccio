8b0310f49c2663133096dfcbf0b8ba7edb0c0cf9
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-11-21 11:29:41 +0100

    Select Animator on interacted Object

--- Testaccio_Unity/Assets/Scripts/Animation/AnimationSpeed.cs
+++ Testaccio_Unity/Assets/Scripts/Animation/AnimationSpeed.cs
@@ -1,22 +1,38 @@
+using System.Collections.Generic;
 using Calculations;
+using UI;
 using UnityEngine;
 
 namespace Animation
 {
     public class AnimationSpeed : MonoBehaviour
     {
-        private float animationSpeed;
-        // Start is called before the first frame update
-        void Start()
-        {
-            animationSpeed = ExtensionMethods.Remap(3, 1, 360, 0, 1);
-            Debug.Log(animationSpeed);
-        }
+        private float animationPosition;
+
+        [SerializeField] private KnobMove activeCircle;
+        [SerializeField] private List<Animator> animators;
+
 
         // Update is called once per frame
         void Update()
         {
-        
+            CalculateAnimationPosition();
+            AndMakeItAlwaysPositive();
+
+        }
+
+        private void CalculateAnimationPosition()
+        {
+            animationPosition = ExtensionMethods.Remap(activeCircle.angle, 1, 360, 0, 1);
+        }
+
+        private void AndMakeItAlwaysPositive()
+        {
+            // Avoid Minus Numbers
+            if (animationPosition < 0)
+            {
+                animationPosition = 0;
+            }
         }
     }
 }
--- Testaccio_Unity/Assets/Scripts/UI/KnobMove.cs
+++ Testaccio_Unity/Assets/Scripts/UI/KnobMove.cs
@@ -1,109 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
-using UI;
-using Unity.VisualScripting;
-using UnityEngine.UIElements;
 
-public class KnobMove : MonoBehaviour
+namespace UI
 {
-    [SerializeField] private GameObject knob;
-    private float knobX;
-    private float knobY;
-    
-    [SerializeField] private float rotationSpeed = 200;
-    private bool stopCircling;
-    private float angle;
-    
-    private float circleRadius;
-    private float knobRadius;
-    private Vector2 centerPos;
-    
-    private RectTransform circleRectTransform;
-    private RectTransform knobRectTransform;
-
-    private KnobPlayerInput knobPlayerInput;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class KnobMove : MonoBehaviour
     {
-        centerPos = transform.position;
-        
-        // Go get the Circle Rect Transform Component
-        circleRectTransform = transform.GetComponent<RectTransform>();
-        
-        // Go get the Knob Rect Transform Component
-        knobRectTransform = knob.GetComponent<RectTransform>();
-        
-        // Links to other Scripts
-        knobPlayerInput = GetComponent<KnobPlayerInput>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        CalculateCircleRadius();
-        CalculateKnobRadius();
-        GetPlayerInputData();
-        CalculateRotationSpeed();
-        KnobToCenterDistance();
-        MoveKnob();
-    }
+        [SerializeField] private GameObject knob;
+        private float knobX;
+        private float knobY;
     
-    private void CalculateKnobRadius()
-    {
-        Vector3 worldscale = knobRectTransform.lossyScale; // Get the local scale factor
-        float worldScaleX = worldscale.x; // only take the x-scale factor and put it into a float
-
-        // Calculate the Radius of the Nob
-        knobRadius = knobRectTransform.sizeDelta.x * 0.5f * worldScaleX; 
-        
-    }
-
-    private void CalculateCircleRadius()
-    {
-        // Calculate Big Circle Radius
-        Vector3 worldscale = circleRectTransform.lossyScale; // Get the local scale factor
-        float worldScaleX = worldscale.x; // only take the x-scale factor and put it into a float
-        
-        // Calculate the Radius of the Circle that has this script attached
-        circleRadius = circleRectTransform.sizeDelta.x * 0.5f * worldScaleX; 
-        
-    }
+        [SerializeField] private float rotationSpeed = 200;
+        private bool stopCircling;
+        [HideInInspector] public float angle;
     
-    private void GetPlayerInputData()
-    {
-        stopCircling = knobPlayerInput.stopCircling;
-    }
+        private float circleRadius;
+        private float knobRadius;
+        private Vector2 centerPos;
+    
+        private RectTransform circleRectTransform;
+        private RectTransform knobRectTransform;
 
-    private void CalculateRotationSpeed()
-    {
-        if (stopCircling)
-            return;
-
-        float distancePerFrame = rotationSpeed * Time.deltaTime;
-
-        
-        angle += (distancePerFrame / circleRadius) * 1000;
-         
-        if (angle > 360.0f)
+        private KnobPlayerInput knobPlayerInput;
+    
+        // Start is called before the first frame update
+        void Start()
         {
-            angle -= 360.0f;
-        }
-    }
-
-    private void KnobToCenterDistance()
-    {
-        // Convert angle to radians (in order to be able to use Sin und Cos)
-        var radians = Mathf.Deg2Rad * angle;
+            centerPos = transform.position;
         
-         knobX = centerPos.x + circleRadius * Mathf.Cos(radians);
-         knobY = centerPos.y + circleRadius * Mathf.Sin(radians);
-    }
+            // Go get the Circle Rect Transform Component
+            circleRectTransform = transform.GetComponent<RectTransform>();
+        
+            // Go get the Knob Rect Transform Component
+            knobRectTransform = knob.GetComponent<RectTransform>();
+        
+            // Links to other Scripts
+            knobPlayerInput = GetComponent<KnobPlayerInput>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CalculateCircleRadius();
+            CalculateKnobRadius();
+            GetPlayerInputData();
+            CalculateRotationSpeed();
+            KnobToCenterDistance();
+            MoveKnob();
+        }
     
-    private void MoveKnob()
-    {
-        knob.transform.position = new Vector3(knobX, knobY, 0);
+        private void CalculateKnobRadius()
+        {
+            Vector3 worldscale = knobRectTransform.lossyScale; // Get the local scale factor
+            float worldScaleX = worldscale.x; // only take the x-scale factor and put it into a float
+
+            // Calculate the Radius of the Nob
+            knobRadius = knobRectTransform.sizeDelta.x * 0.5f * worldScaleX; 
+        
+        }
+
+        private void CalculateCircleRadius()
+        {
+            // Calculate Big Circle Radius
+            Vector3 worldscale = circleRectTransform.lossyScale; // Get the local scale factor
+            float worldScaleX = worldscale.x; // only take the x-scale factor and put it into a float
+        
+            // Calculate the Radius of the Circle that has this script attached
+            circleRadius = circleRectTransform.sizeDelta.x * 0.5f * worldScaleX; 
+        
+        }
+    
+        private void GetPlayerInputData()
+        {
+            stopCircling = knobPlayerInput.stopCircling;
+        }
+
+        private void CalculateRotationSpeed()
+        {
+            if (stopCircling)
+                return;
+
+            float distancePerFrame = rotationSpeed * Time.deltaTime;
+
+        
+            angle += (distancePerFrame / circleRadius) * 1000;
+         
+            if (angle > 360.0f)
+            {
+                angle -= 360.0f;
+            }
+        }
+
+        private void KnobToCenterDistance()
+        {
+            // Convert angle to radians (in order to be able to use Sin und Cos)
+            var radians = Mathf.Deg2Rad * angle;
+        
+            knobX = centerPos.x + circleRadius * Mathf.Cos(radians);
+            knobY = centerPos.y + circleRadius * Mathf.Sin(radians);
+        }
+    
+        private void MoveKnob()
+        {
+            knob.transform.position = new Vector3(knobX, knobY, 0);
+        }
     }
 }
