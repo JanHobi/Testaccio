d45d36b1365c2abb891b0a0f394b91028cbc30e1
JanHobi <jan.hobi@hotmail.com>
2023-12-05 17:42:09 +0100

    added string, shark animation

--- Testaccio_Unity/Assets/Scripts/Animation/Destroyable.cs
+++ Testaccio_Unity/Assets/Scripts/Animation/Destroyable.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroyable : MonoBehaviour
-{
+{    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bridge"))
         {
             DestroyMe();
         }
+
     }
 
     public void DestroyMe() { Destroy(gameObject); }
+
+    public void DestroyMeAfterTime() { Destroy(gameObject, 1); }
 }
