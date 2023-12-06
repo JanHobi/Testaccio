ede45504ad6d1acaedefd9eb12e01361cd1370d4
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-12-06 12:25:37 +0100

    Generate and Show Tasks on screen

--- Testaccio_Unity/Assets/Scripts/Managers/TaskManager.cs
+++ Testaccio_Unity/Assets/Scripts/Managers/TaskManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 
 namespace Managers
@@ -10,24 +11,115 @@ namespace Managers
     public class TaskManager : MonoBehaviour
     {
         [SerializeField] private List<string> allTasks = new List<string>();
-        [HideInInspector] public List<string> selectedTasks = new List<string>();
-        [HideInInspector] public Dictionary<string, bool> TasksAndBools = new Dictionary<string, bool>();
-        
-        
-        private void Awake()
+        private Dictionary<string, bool> SelectedTasks = new Dictionary<string, bool>();
+        [SerializeField] private int numberOfTasksShown;
+         private float spacing = 30;
+        [SerializeField] private TMP_Text uiTaskPrefab;
+        [SerializeField] private Transform taskParent;
+      
+
+       
+        private static TaskManager _instance;
+
+        public static TaskManager Instance
         {
-            foreach (var task in allTasks)
+            get
             {
-                TasksAndBools.Add(task, false);
+                if (_instance == null)
+                {
+                    Debug.Log("Taskmanager is null!");
+                }
+
+                return _instance;
             }
         }
+
+        private void Awake()
+        {
+            _instance = this;
+        }
         
+       
+
         void Start()
         {
-            // Generate three random indices and add corresponding tasks to selectedTasks
-            selectedTasks = Enumerable.Range(0, 3)
-                .Select(_ => allTasks[Random.Range(0, TasksAndBools.Count)])
-                .ToList();
+            NewTasks();
+        }
+
+        public void CheckTasksCompletion()
+        {
+            bool allTasksCompleted = SelectedTasks.All(task => task.Value == true);
+
+            if (allTasksCompleted)
+            {
+                // All tasks are completed
+                Debug.Log("All tasks completed!");
+            }
+        }
+
+        public Dictionary<string, bool> CurrentTasks()
+        {
+            return SelectedTasks;
+        }
+
+        [ContextMenu("Spawn New Tasks")]
+        public void NewTasks()
+        {
+            // Clear existing UI elements
+            ClearUI();
+            
+            // Clear dictionary
+            SelectedTasks.Clear();
+            
+            // Generate new tasks from Pool and then display them
+            SelectedTasks = GetTasksFromPool();
+            ShowTasksAsUI();
+        }
+
+        private void ClearUI()
+        {
+            foreach (Transform child in taskParent)
+            {
+               Destroy(child.gameObject);
+            }
+        }
+
+        private Dictionary<string, bool> GetTasksFromPool()
+        {
+            if (numberOfTasksShown > allTasks.Count)
+            {
+                numberOfTasksShown = allTasks.Count;
+            }
+            
+            for (int i = 0; i < numberOfTasksShown; i++)
+            {
+                int randomIndex = Random.Range(0, numberOfTasksShown);
+                string selectedTask = allTasks[randomIndex];
+                
+                // Add to dictionary
+                SelectedTasks.Add(selectedTask, false);
+                
+                // Remove the new found tasks from the Pool of possible tasks
+                allTasks.RemoveAt(randomIndex);
+            }
+            return (SelectedTasks);
+        }
+        
+        private void ShowTasksAsUI()
+        {
+           
+            float yPosOffset = 0f;
+            
+            foreach (var tasks in SelectedTasks.Keys)
+            {
+                TMP_Text newtext = Instantiate(uiTaskPrefab, taskParent);
+                newtext.text = tasks;
+
+                var position = uiTaskPrefab.transform.position;
+                newtext.rectTransform.anchoredPosition = new Vector2(position.x, position.y -yPosOffset);
+
+                yPosOffset += spacing;
+            }
         }
     }
 }
