fb56e76f3b284174e94033f1de5bc486d87d80a8
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-12-06 14:38:23 +0100

    Move to namespace

--- Testaccio_Unity/Assets/Scripts/Animation/Destroyable.cs
+++ Testaccio_Unity/Assets/Scripts/Animation/Destroyable.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using Managers;
 using UnityEngine;
 
-public class Destroyable : MonoBehaviour
-{    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Bridge"))
+namespace Animation
+{
+    public class Destroyable : MonoBehaviour
+    {    
+        private void OnTriggerEnter(Collider other)
         {
-            DestroyMe();
-            TaskManager.Instance.SetTaskToDone("Heavy Bridge");
+            if (other.gameObject.CompareTag("Bridge"))
+            {
+                DestroyMe();
+                TaskManager.Instance.SetTaskToDone("Heavy Bridge");
+            }
+
         }
 
+        public void DestroyMe() { Destroy(gameObject); }
+
+        public void DestroyMeAfterTime() { Destroy(gameObject, 1); }
     }
-
-    public void DestroyMe() { Destroy(gameObject); }
-
-    public void DestroyMeAfterTime() { Destroy(gameObject, 1); }
 }
