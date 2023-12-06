82bb68c233d8f4622b76f4aac810644ed0a7e8ab
dominicsuttero <79805996+dominicsuttero@users.noreply.github.com>
2023-12-06 17:06:23 +0100

    Continue on SoundManager script

--- Testaccio_Unity/Assets/Scripts/Audio/AudioManager.cs
+++ Testaccio_Unity/Assets/Scripts/Audio/AudioManager.cs
@@ -1,11 +1,17 @@
 using System;
 using UnityEngine;
+using FMOD;
+using FMODUnity;
+using Debug = UnityEngine.Debug;
 
 namespace Audio
 {
     public class AudioManager : MonoBehaviour
     {
         private static AudioManager _instance;
+        private FMOD.Studio.EventInstance menuMusic = RuntimeManager.CreateInstance("event/menuMusic");
+        private FMOD.Studio.EventInstance gameMusic = RuntimeManager.CreateInstance("event/gameMusic");
+        
         public static AudioManager Instance
         {
             get
@@ -22,6 +28,15 @@ namespace Audio
         private void Awake()
         {
             _instance = this;
+        }
+
+        public void PlayMenuMusic()
+        {
+            menuMusic.start();
+        }
+        public void PlayGameMusic()
+        {
+            gameMusic.start();
         }
     }
 }
