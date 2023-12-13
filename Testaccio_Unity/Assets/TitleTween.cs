42a16fb4b3e4c967bc11d675a10350d4cc629ae5
JanHobi <jan.hobi@hotmail.com>
2023-12-13 14:46:44 +0100

    added clean MainScene

--- Testaccio_Unity/Assets/TitleTween.cs
+++ Testaccio_Unity/Assets/TitleTween.cs
@@ -14,14 +14,33 @@ public class TitleTween : MonoBehaviour
 
     private void Start()
     {
-        TweenTitle();
+        if (!isTweened)
+        {
+            title.transform.DOShakePosition(0.5f, 5f, 20, 90f, false, false).SetDelay(1f).OnComplete(() => 
+            {    
+                accidents.rectTransform.DOAnchorPosY(-15, 0.5f).SetEase(Ease.OutBack);
+
+                edition.rectTransform.DOScale(1, 0.25f).SetEase(Ease.InQuad);
+
+                edition.DOFade(1, 0.25f).SetEase(Ease.InQuad).OnComplete(() => 
+                {
+                    edition.rectTransform.DOScale(1.03f, 1f).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
+                    isTweened = true;
+                });
+            });
+            
+        }
+        else 
+        {
+            return;
+        }
     }
 
     public void TweenTitle()
     {
         if (!isTweened)
         {
-            title.transform.DOShakePosition(0.5f, 5f, 20, 90f, false, false).SetDelay(1f).OnComplete(() => 
+            title.transform.DOShakePosition(0.5f, 5f, 20, 90f, false, false).SetDelay(0f).OnComplete(() => 
             {    
                 accidents.rectTransform.DOAnchorPosY(-15, 0.5f).SetEase(Ease.OutBack);
 
