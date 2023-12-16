ea77159c98d693ae3bd7377c0f5865aa9bdd88f7
JanHobi <jan.hobi@hotmail.com>
2023-12-16 12:37:49 +0100

    market sign change and some more mesh fixes

--- Testaccio_Unity/Assets/Scripts/Animation/MarketAnimationCheck.cs
+++ Testaccio_Unity/Assets/Scripts/Animation/MarketAnimationCheck.cs
@@ -6,10 +6,14 @@ namespace Animation
     public class MarketAnimationCheck : MonoBehaviour
     {
         [SerializeField] private GameObject marketFish;
+        [SerializeField] private GameObject marketSignFreshFish;
+        [SerializeField] private GameObject marketSignSoldOut;
 
         private void Start()
         {
             marketFish.SetActive(false);
+            marketSignFreshFish.SetActive(false);
+            marketSignSoldOut.SetActive(true);
         }
 
         private void OnTriggerEnter(Collider other)
@@ -17,6 +21,8 @@ namespace Animation
             if (other.gameObject.CompareTag("Fisher"))
             {
                 marketFish.SetActive(true);
+                marketSignFreshFish.SetActive(true);
+                marketSignSoldOut.SetActive(false);
             
                 // Task Done
                 TaskManager.Instance.SetTaskToDone("Fish to Market");
@@ -25,6 +31,8 @@ namespace Animation
             if (other.gameObject.CompareTag("Passenger"))
             {
                 if (!marketFish.activeSelf) return;
+                marketSignFreshFish.SetActive(false);
+                marketSignSoldOut.SetActive(true);
                 // Task Done
                 TaskManager.Instance.SetTaskToDone("Comfort Dinner");
                 
